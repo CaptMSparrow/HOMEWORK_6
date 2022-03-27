600f7e598e44132b638d0127b91568c9eefae8e0
CaptMSparrow <meirri1237@gmail.com>
2022-03-27 13:39:36 +0300

    task1 changed

--- task1.py
+++ task1.py
@@ -1,12 +1,8 @@
 def concat(*args: str, reversed=False, sep=' '):
-    words = []
-    for word in args:
-        words.append(word)
+    words = [word for word in args]
     if reversed:
         words.reverse()
-        print(*words)
-    else:
-        print(*words)
+    print(*words)
 
 
-concat('Hi', "I'm", 'Mary', reversed=True)
+concat('Hi', "I'm", 'Mary', reversed=False)
